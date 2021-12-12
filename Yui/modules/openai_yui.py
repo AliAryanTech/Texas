4aa47eb51d43d102f1b5f25e2c88f3e1ce32d8b0
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2021-12-12 17:18:51 +0500

    Update openai_yui.py

--- Yui/modules/openai_yui.py
+++ Yui/modules/openai_yui.py
@@ -31,7 +31,7 @@ class Yui_OpenAI():
     async def ask_yui(self, question, chat_log=None):
         if not chat_log:
             chat_log = defaults.Chat_Log
-        prmpt = f"{chat_log}You: {question}\nTexαs:"
+        prmpt = f"{chat_log}You: {question}\nTexas:"
         response = completion.create(
             prompt=prmpt, engine=self.engine, stop=["\nYou"], temperature=0.9,
             top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
